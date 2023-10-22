2325ef1cc10cd1e4ce28d00d519f6df61583b7f3
DKOLABS <dillon.obrien@dkolabs.com>
2023-10-22 17:31:20 -0400

    Updates

--- dump_ca_policies.ps1
+++ dump_ca_policies.ps1
@@ -1,10 +1,11 @@
-$ClientSecretCredential = Get-Credential -Credential ""
-Connect-MgGraph -TenantId "" -ClientSecretCredential $ClientSecretCredential -NoWelcome
+# Connect to Microsoft Graph
+Connect-MgGraph -NoWelcome
+
 # Fetch conditional access policies
 $policies = Get-MgIdentityConditionalAccessPolicy
 
 # Get the current date and time
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
 
 # Start the HTML content
 $htmlContent = @"
@@ -34,6 +35,12 @@ $htmlContent = @"
             margin-top: 20px; /* Added margin for space */
         }
         .policy h3.grant-controls {
+            margin-top: 20px; /* Added margin for space */
+        }
+        .policy h3.session-controls {
+            margin-top: 20px; /* Added margin for space */
+        }
+        .policy h3.raw-json {
             margin-top: 20px; /* Added margin for space */
         }
         .policy h3.collapsed::before {
@@ -111,6 +118,10 @@ foreach ($policy in $policies) {
                     <td>$($policy.modifiedDateTime)</td>
                 </tr>
                 <tr>
+                    <td>Description</td>
+                    <td>$($policy.description)</td>
+                </tr>
+                <tr>
                     <td>State</td>
                     <td>$($policy.state)</td>
                 </tr>
@@ -134,8 +145,42 @@ foreach ($policy in $policies) {
                         <td>Sign-in Risk Levels</td>
                         <td>$($policy.conditions.signInRiskLevels -join ', ')</td>
                     </tr>
+                    <tr>
+                        <td>User Risk Levels</td>
+                        <td>$($policy.conditions.userRiskLevels -join ', ')</td>
+                    </tr>
                 </table>
                 <br>
+
+                <!-- Applications Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Applications</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Included Applications</td>
+                            <td>$($policy.conditions.applications.includeApplications -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Applications</td>
+                            <td>$($policy.conditions.applications.excludeApplications -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Include Authentication Context Class References</td>
+                            <td>$($policy.conditions.applications.includeAuthenticationContextClassReferences -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Include User Actions</td>
+                            <td>$($policy.conditions.applications.includeUserActions -join ', ')</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+
+                <!-- Users Sub-Section -->
                 <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Users</h3>
                 <div class="content sub-section">
                     <table>
@@ -144,18 +189,122 @@ foreach ($policy in $policies) {
                             <th>Value</th>
                         </tr>
                         <tr>
-                            <td>Include Users</td>
+                            <td>Included Users</td>
                             <td>$($policy.conditions.users.includeUsers -join ', ')</td>
                         </tr>
                         <tr>
-                            <td>Exclude Users</td>
+                            <td>Excluded Users</td>
                             <td>$($policy.conditions.users.excludeUsers -join ', ')</td>
                         </tr>
-                        <!-- You can continue with other attributes of 'conditions.users' as needed -->
+                        <tr>
+                            <td>Included Groups</td>
+                            <td>$($policy.conditions.users.includeGroups -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Groups</td>
+                            <td>$($policy.conditions.users.excludeGroups -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Included Roles</td>
+                            <td>$($policy.conditions.users.includeRoles -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Roles</td>
+                            <td>$($policy.conditions.users.excludeRoles -join ', ')</td>
+                        </tr>
                     </table>
+                </div>
+                <br>
+
+                <!-- Device Filter Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Device Filter</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Mode</th>
+                            <th>Rule</th>
+                        </tr>
+                        <tr>
+                            <td>$($policy.conditions.devices.deviceFilter.mode)</td>
+                            <td>$($policy.conditions.devices.deviceFilter.rule)</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+
+                <!-- Locations Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Locations</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Included Locations</td>
+                            <td>$($policy.conditions.locations.includeLocations -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Locations</td>
+                            <td>$($policy.conditions.locations.excludeLocations -join ', ')</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+
+                <!-- Platforms Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Platforms</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Included Platforms</td>
+                            <td>$($policy.conditions.platforms.includePlatforms -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Platforms</td>
+                            <td>$($policy.conditions.platforms.excludePlatforms -join ', ')</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+
+                <!-- Client Applications Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Client Applications</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Included Service Principals</td>
+                            <td>$($policy.conditions.clientApplications.includeServicePrincipals -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Excluded Service Principals</td>
+                            <td>$($policy.conditions.clientApplications.excludeServicePrincipals -join ', ')</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+
+                <!-- ClientAppTypes Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Client App Types</h3>
+                <div class="content sub-section">
+                    <ul>
+                        $(foreach ($item in $policy.conditions.clientAppTypes) {
+                        "<li>$($item)</li>"
+                            }
+                        )
+                    </ul>
                 </div>
             </div>
             <br>
+
             <h3 class="collapsed grant-controls" onclick="toggleContent(this)">Grant Controls</h3>
             <div class="content">
                 <table>
@@ -172,10 +321,130 @@ foreach ($policy in $policies) {
                         <td>$($policy.grantControls.builtInControls -join ', ')</td>
                     </tr>
                     <tr>
-                        <td>Custom Controls</td>
-                        <td>$($policy.grantControls.customControls -join ', ')</td>
+                        <td>Custom Authentication Factors</td>
+                        <td>$($policy.grantControls.customAuthenticationFactors -join ', ')</td>
+                    </tr>
+                    <tr>
+                        <td>Terms of Use</td>
+                        <td>$($policy.grantControls.termsOfUse -join ', ')</td>
                     </tr>
                 </table>
+                <br>
+
+                <!-- Authentication Strength Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Authentication Strength</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>ID</td>
+                            <td>$($policy.grantControls.authenticationStrength.id -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Disaply Name</td>
+                            <td>$($policy.grantControls.authenticationStrength.displayName -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Description</td>
+                            <td>$($policy.grantControls.authenticationStrength.description -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Policy Type</td>
+                            <td>$($policy.grantControls.authenticationStrength.policyType -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Created Datetime</td>
+                            <td>$($policy.grantControls.authenticationStrength.createdDateTime -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Modified Datetime</td>
+                            <td>$($policy.grantControls.authenticationStrength.modifiedDateTime -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Allowed Combinations</td>
+                            <td>$($policy.grantControls.authenticationStrength.allowedCombinations -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Combination Configurations</td>
+                            <td>$($policy.grantControls.authenticationStrength.combinationConfigurations -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Requirements Satisfied</td>
+                            <td>$($policy.grantControls.authenticationStrength.requirementsSatisfied -join ', ')</td>
+                        </tr>
+
+                    </table>
+                </div>
+                <br>
+            </div>
+            <br>
+
+            <h3 class="collapsed session-controls" onclick="toggleContent(this)">Session Controls</h3>
+            <div class="content">
+                <table>
+                    <tr>
+                        <th>Attribute</th>
+                        <th>Value</th>
+                    </tr>
+                    <tr>
+                        <td>Application Enforced Restrictions</td>
+                        <td>$($policy.sessionControls.applicationEnforcedRestrictions)</td>
+                    </tr>
+                    <tr>
+                        <td>Cloud App Security</td>
+                        <td>$($policy.sessionControls.cloudAppSecurity.isEnabled)</td>
+                    </tr>
+                    <tr>
+                        <td>Disable Resilience Defaults</td>
+                        <td>$($policy.sessionControls.disableResilienceDefaults)</td>
+                    </tr>
+                    <tr>
+                        <td>Persistent Browser</td>
+                        <td>$($policy.sessionControls.persistentBrowser.isEnabled)</td>
+                    </tr>
+                </table>
+                <br>
+
+                <!-- Sign In Frequency Sub-section -->
+                <h3 class="collapsed sub-header" onclick="toggleContent(this, event)">Sign In Frequency</h3>
+                <div class="content sub-section">
+                    <table>
+                        <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Authentication Type</td>
+                            <td>$($policy.sessionControls.signInFrequency.authenticationType -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Frequency Interval</td>
+                            <td>$($policy.sessionControls.signInFrequency.frequencyInterval -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Enabled</td>
+                            <td>$($policy.sessionControls.signInFrequency.isEnabled -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Type</td>
+                            <td>$($policy.sessionControls.signInFrequency.type -join ', ')</td>
+                        </tr>
+                        <tr>
+                            <td>Value</td>
+                            <td>$($policy.sessionControls.signInFrequency.value -join ', ')</td>
+                        </tr>
+                    </table>
+                </div>
+                <br>
+            </div>
+            <br>
+
+            <h3 class="collapsed raw-json" onclick="toggleContent(this)">Raw JSON</h3>
+            <div class="content">
+                <pre>$($policy.ToJsonString())</pre>
             </div>
         </div>
     </div>
